b527e1f5773bd47b233d7d857f39f7258991acbd
兰陵王 <gemiman@vip.qq.com>
2026-05-19 21:57:02 +0800

    feat: 启用 axum 宏并重构路由处理

--- example/web-starter/src/main.rs
+++ example/web-starter/src/main.rs
@@ -1,13 +1,18 @@
-use axum::{Router, routing};
+use axum::{Router, debug_handler, routing};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", routing::get(async || "Hello Daoyi Cloud Axum !"));
+    let router = Router::new().route("/", routing::get(index));
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     println!("Listening on http://0.0.0.0:3000");
 
     axum::serve(listener, router).await.unwrap();
+}
+
+#[debug_handler]
+async fn index() -> &'static str {
+    "Hello Daoyi Cloud Axum !"
 }
